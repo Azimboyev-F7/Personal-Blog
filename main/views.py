31fc3df16401acb2572bd058125319f6df7f71e2
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-04-13 21:39:09 +0500

    modified

--- main/views.py
+++ main/views.py
@@ -8,3 +8,13 @@ def index(request):
     context = {}    
 
     return render(request, 'index.html', context)
+
+def about(request):
+    context = {}    
+
+    return render(request, 'about.html', context)
+
+def contact(request):
+    context = {}    
+
+    return render(request, 'contact.html', context)
