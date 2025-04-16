c2bf56336f878a1d1fa41e541a2890179c70dfa4
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-04-16 21:23:10 +0500

    modified

--- main/views.py
+++ main/views.py
@@ -7,14 +7,14 @@ from django.shortcuts import render
 def index(request):
     context = {}    
 
-    return render(request, 'index.html', context)
+    return render(request, 'article/index.html', context)
 
 def about(request):
     context = {}    
 
-    return render(request, 'about.html', context)
+    return render(request, 'article/about.html', context)
 
 def contact(request):
     context = {}    
 
-    return render(request, 'contact.html', context)
+    return render(request, 'article/contact.html', context)
